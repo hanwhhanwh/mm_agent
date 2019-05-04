18e76f5a60b5d1cdfee08f8cfd436af0b3fdb219
hbesthee <hbesthee@naver.com>
2019-05-04 17:41:02 +0900

    LAST_INSERT_ID() 를 이용한 Master/Detail 삽입 예제

--- config/LAST_INSERT_ID.example.sql
+++ config/LAST_INSERT_ID.example.sql
@@ -5,7 +5,14 @@ CREATE TABLE IF NOT EXISTS LOG_TEST
 	, PRIMARY KEY ( log_no )
 );
 
-
+-- DROP TABLE IF EXISTS LOG_TEST_DETAIL;
+CREATE TABLE IF NOT EXISTS LOG_TEST_DETAIL
+(
+	log_detail_no INT NOT NULL AUTO_INCREMENT 
+	, log_no INT NOT NULL 
+	, value INT NULL
+	, PRIMARY KEY ( log_detail_no )
+);
 
 
 SET @log_no = 0;
@@ -17,12 +24,13 @@ SET @log_no = LAST_INSERT_ID();
 SELECT @log_no;
 
 
--- SELECT @log_on;
+
 
 SET @log_no = 0;
 
+INSERT INTO LOG_TEST ( log_date ) VALUES ( NOW() );
+
 SET @log_no = LAST_INSERT_ID();
 
-SELECT @log_no; -- LAST_INSERT_ID();
-
+INSERT INTO LOG_TEST_DETAIL ( log_no, value ) VALUES ( @log_no, 0), ( @log_no, 1), ( @log_no, 2)
 
