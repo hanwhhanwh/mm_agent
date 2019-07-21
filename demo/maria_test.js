b84da19f386548637b7fb781acc36f24943a312f
hbesthee <hbesthee@naver.com>
2019-07-21 17:55:33 +0900

    mod: mysql test code

--- demo/maria_test.js
+++ demo/maria_test.js
@@ -10,4 +10,23 @@ const mariadb = require("../lib/mariadb.js");
 
 //console.log("config => " + config.host);
 
+mariadb.getConnection(function(err, connection) {
+    if (err) {
+        throw err;
+        // console.log("Pool.getConnection error");
+        // return;
+    }
+
+    connection.query('SELECT * FROM COINS', function(err, rows, fields) {
+        if (!err){  
+        //response.send(rows);   
+        console.log('The solution is: ', rows[0]);  
+        console.log('fields is: ', fields[0]);  
+        }  
+        else  
+        console.log('Error while performing Query.');  
+        connection.release();  
+    });  
+    mariadb.end();
+});
 
