3c782b3cbfe470c3200b9a5935af8c069ed221eb
Wonhee <hanwhhanwh@gmail.com>
2021-05-21 01:08:30 +0900

    add mysql_demo

--- demo/mysql_demo.js
+++ demo/mysql_demo.js
@@ -7,18 +7,62 @@ reference : https://github.com/mysqljs/mysql
 */
 
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
-});
+const db_config = require('../config/db.json')
+var connection = mysql.createConnection(db_config);
+
+console.log(db_config.host);
 
 connection.connect();
 
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
+  console.log(results);
+  console.log(fields);
 });
 
 connection.end();
+
+
+function insertRecentCredits (coin_no, rows)
+{
+	var connection = mysql.createConnection(db_config);
+	connection.connect();
+
+	let query = "";
+	rows.forEach( (row, index) => {
+		query = "\
+INSERT INTO `RECENT_CREDITS` (`reg_date`, `coin_no`, `amount`) VALUES\
+	('" + row.date + "', " + coin_no + ", " + row.amount + ")\
+ON DUPLICATE KEY UPDATE\
+	amount = " + row.amount + "\
+	, `last_update` = NOW()\
+;\n";
+		connection.query(query, function (error, results, fields) {
+			if (error)
+				throw error;
+		});
+	});
+	connection.end();
+}
+
+
+function insertData(qeury)
+{
+	var connection = mysql.createConnection(db_config);
+	connection.connect({multipleStatements: true});
+
+	connection.query(qeury, function (error, results, fields) {
+		if (error)
+			throw error;
+		console.log(results);
+		console.log(fields);
+	});
+
+	connection.end();
+}
+
+exports.insertRecentCredits = insertRecentCredits;
+exports.insertData = insertData;
+module.exports = exports;
+
