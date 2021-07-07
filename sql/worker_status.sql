988f619fd462fd22f0334d5e7f4de0effd6c3789
Wonhee <hanwhhanwh@gmail.com>
2021-07-07 18:02:31 +0900

    상태정보 이쁘게 보이게 수정

--- sql/worker_status.sql
+++ sql/worker_status.sql
@@ -1,6 +1,6 @@
 -- 코인별 최근 수집 현황
 SELECT
-	R.reg_date, N.user_name, D.coin_no, D.hashrate, D.worker_no, D.work_status_no
+	R.reg_date, D.coin_no, C.acronyms, N.user_name, D.hashrate, D.worker_no, D.work_status_no
 FROM WORKERS_STATUS_DATA AS D
 	INNER JOIN (
 		SELECT
@@ -11,14 +11,16 @@ FROM WORKERS_STATUS_DATA AS D
 	) AS R ON R.coin_no = D.coin_no
 		AND R.work_status_no = D.work_status_no
 	INNER JOIN WORKER AS N ON N.worker_no = D.worker_no
+	INNER JOIN COIN AS C ON C.coin_no = D.coin_no
 WHERE 1 = 1
 	AND N.is_alert = 1
-ORDER BY D.coin_no, 2, D.work_status_no DESC, worker_no
+ORDER BY 4, D.coin_no, 2, D.work_status_no DESC, worker_no
 LIMIT 200;
 
 
 SELECT
-	*
-FROM RECENT_CREDITS
-ORDER BY reg_date DESC
+	R.reg_date, R.coin_no, C.acronyms, R.amount, R.last_update
+FROM RECENT_CREDITS AS R
+	INNER JOIN COIN AS C ON C.coin_no = R.coin_no
+ORDER BY R.reg_date DESC, R.coin_no
 LIMIT 12;
