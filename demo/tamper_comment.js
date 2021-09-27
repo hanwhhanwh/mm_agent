00944feb8f57e75317035b6953c4554b0ab70ffe
Wonhee Han <hanwhhanwh@gmail.com>
2021-09-27 10:39:27 +0900

    mod: comment button

--- demo/tamper_comment.js
+++ demo/tamper_comment.js
@@ -5,36 +5,49 @@ let CommentMain = {
 				let elmTarget = o.querySelector(".view-good"),
 					elmCommentBtn = o.querySelector("#my_comment_btn");
 				if (null == elmCommentBtn) {
-					if (null == elmTarget)
-						elmTarget = o.querySelector(".view-good-box").parentNode;
+					if (null == elmTarget) {
+						elmTarget = o.querySelector(".view-good-box").firstChild;
+					}
 
 					if (null != elmTarget) {
 						elmCommentBtn = CommentMain.NewCommentButton();
-						elmTarget.parentNode.insertBefore(elmCommentBtn, elmTarget) 
+						elmTarget.parentNode.insertBefore(elmCommentBtn, elmTarget) ;
 					}
 				}
-				if (null != elmCommentBtn)
-					console.log("CommentMain : add NewCommentButton.")
+				if (null != elmCommentBtn) {
+					console.log("CommentMain : add NewCommentButton.");
+				}
 			}
-			return !0
+			return !0;
 		} catch (e) {
-			console.log("CommentMain.DocOnLoad. ", e)
+			console.log("CommentMain.DocOnLoad. ", e);
 		}
-	},
-	WaitLoadDom: function (o) {
-		CommentMain.vid = CommentMain.getVid(o), CommentMain.vid ? null != o.querySelector("#info #menu-container") ? CommentMain.DocOnLoad(o) : setTimeout(function () {
-			CommentMain.WaitLoadDom(o)
-		}, 1e3) : CommentMain.checkChangeVid()
 	},
 	NewCommentButton: function () {
 		try {
-			var o = document.createElement("button");
-			return o.id = "y2mateconverter", o.className = "yt-uix-tooltip", o.setAttribute("type", "button"), o.setAttribute("title", "Download with y2mate.com"), o.innerHTML = "Download", o.addEventListener("click", function (o) {
-				CommentMain.goToY2mate(o)
-			}, !0), o.setAttribute("style", "min-height:25px; position:relative; top:1px; cursor: pointer; font: 13px Arial; background: #ff003e; color: #fff; text-transform: uppercase; display: block; padding: 10px 16px; margin: 20px 5px 10px 5px; border: 1px solid #ff0068; border-radius: 2px; font-weight:bold"), o.setAttribute("onmouseover", "this.style.backgroundColor='#c10841'"), o.setAttribute("onmouseout", "this.style.backgroundColor='#ff003e'"), o
-		} catch (o) {
-			console.log("Ошибка в функции Y2mate.GetCommandButton. ", o)
+			let elmNewButton = document.createElement("button");
+			elmNewButton.id = "my_comment_btn"; elmNewButton.className = "comment-btn-tooltip";
+			elmNewButton.setAttribute("type", "button");
+			elmNewButton.setAttribute("title", "Comment");
+			elmNewButton.innerHTML = "Comment";
+			elmNewButton.setAttribute("style", "min-height:25px; position:relative; top:1px; cursor: pointer; font: 13px Arial; background: #ff003e; color: #fff; text-transform: uppercase; display: block; padding: 10px 16px; margin: 20px 5px 10px 5px; border: 1px solid #ff0068; border-radius: 2px; font-weight:bold");
+			elmNewButton.setAttribute("onmouseover", "this.style.backgroundColor='#c10841'");
+			elmNewButton.setAttribute("onmouseout", "this.style.backgroundColor='#ff003e'");
+			elmNewButton.addEventListener("click", function (a) {
+				let elmComment = document.querySelector('#wr_content')
+                if (document.location.href.indexOf('realddr') >= 0) {
+                    elmComment.value = '리뷰 잘 보고 갑니다.';
+                }
+                else {
+                    elmComment.value = '좋은 자료 감사합니다.';
+                }
+				apms_comment('viewcomment');
+				elmComment.scrollIntoView();
+			}, !0);
+			return elmNewButton;
+		} catch (e) {
+			console.log("NewCommentButton has error. ", e)
 		}
 	}
 };
-CommentMain.WaitLoadDom(window.document);
+CommentMain.DocOnLoad(window.document);
